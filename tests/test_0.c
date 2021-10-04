d9ed294d7beb53fb3d9d40433e875ab9a4087220
Noah Chernet <noahchernet@yahoo.com>
2021-10-04 22:08:16 +0300

    Updated tests to include more variations and integers to be printed

--- tests/test_0.c
+++ tests/test_0.c
@@ -3,16 +3,14 @@
 
 int main(void)
 {
-	int x = _printf("Hello, %%%s%c%c%c", "World", '!', '\n', '9');
-	int y = printf("Hello, %%%s%c%c%c", "World", '!', '\n', '9');
+	int x = _printf("Hello, %%%s%c%c%c\n", "World", '!', '\n', '9');
+	int y = printf("Hello, %%%s%c%c%c\n", "World", '!', '\n', '9');
 
 	printf("x = %d\n", x);
 	printf("y = %d\n", y);
 
-	x = _printf("%c%s%%%c%s\n", 'H', NULL, 'W', "orld!");
-/*
-	y = printf("%c%s%%%c%s\n", 'H', NULL, 'W', "orld!");
-*/
+	x = _printf("%c%%%c%s\n", 'H', 'W', "orld!");
+	y = printf("%c%%%c%s\n", 'H', 'W', "orld!");
 
 	printf("x = %d\n", x);
 	printf("y = %d\n", y);
